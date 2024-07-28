c3dd563333164b9703c5c965e56558c0d55f6293
Jim Nicholls <jim.nicholls@gmail.com>
2024-07-28 13:42:05 +1000

    Update mepc from the value returned by the trap handler.

--- src/kernel/traps.c
+++ src/kernel/traps.c
@@ -2,7 +2,7 @@
 
 
 [[gnu::nonnull, gnu::returns_nonnull]]
-void trap_handler(uint64_t cause, uint64_t tval, void* const pc, uint64_t a_regs[8]) {
+void* trap_handler(uint64_t cause, uint64_t tval, void* const pc, uint64_t a_regs[8]) {
     /* Context has already been saved, and will be restored when trap_handler exists. See trap_handler_0 in traps.S */
     for (;;);
 }
