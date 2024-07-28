a6229376edc48a6d4782fe422edf0d07ae62e9cd
Jim Nicholls <jim.nicholls@gmail.com>
2024-07-28 13:39:16 +1000

    Pass mcause, mtval and pointer to the a regs to the trap handler.

--- src/kernel/traps.c
+++ src/kernel/traps.c
@@ -1,4 +1,8 @@
-[[gnu::aligned(4), gnu::interrupt, gnu::visibility("hidden")]]
-void trap_handler(void) {
-    for(;;);
+#include <stdint.h>
+
+
+[[gnu::nonnull, gnu::returns_nonnull]]
+void trap_handler(uint64_t cause, uint64_t tval, void* const pc, uint64_t a_regs[8]) {
+    /* Context has already been saved, and will be restored when trap_handler exists. See trap_handler_0 in traps.S */
+    for (;;);
 }
